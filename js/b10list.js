1733408211cb2ddcf6903ec1629d46f894af47e8
ght <597315261@qq.com>
2018-03-15 18:50:16 +0800

    修正01的路径

--- js/b10list.js
+++ js/b10list.js
@@ -5,7 +5,8 @@ const defaultArtist = "匿名 试听版";
 const playerlist = [
     {
         name: '演员',
-        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b1001%E6%BC%94%E5%91%98.flac'
+        artist: "匿名 试听版",
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/01%E6%BC%94%E5%91%98.flac'
     },
     {
         name: '双生花',
