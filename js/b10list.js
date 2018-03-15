c9288a58043a72a25e72f50fcb222d5ca566cd72
ght <597315261@qq.com>
2018-03-15 14:06:52 +0800

    更新标题图 更新参赛歌曲

--- js/b10list.js
+++ js/b10list.js
@@ -1,20 +1,114 @@
-const coverUrl = "http://7xp06y.com1.z0.glb.clouddn.com/music1cover.jpg";
-const defaultArtist = "佚名";
+const coverUrl = "http://7xp06y.com1.z0.glb.clouddn.com/b10/FXF48logo.png";
+const defaultArtist = "匿名 试听版";
 
 
-const playerlist = [{
-    name: 'start dash!',
-    url: 'http://7xp06y.com1.z0.glb.clouddn.com/music1.mp3'
-}, {
-    name: 'start dash!',
-    url: 'http://7xp06y.com1.z0.glb.clouddn.com/music1.mp3'
-}, {
-    name: 'start dash!',
-    url: 'http://7xp06y.com1.z0.glb.clouddn.com/music1.mp3'
-}, {
-    name: 'start dash!',
-    url: 'http://7xp06y.com1.z0.glb.clouddn.com/music1.mp3'
-}];
+const playerlist = [
+    {
+        name: '演员',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b1001%E6%BC%94%E5%91%98.flac'
+    },
+    {
+        name: '双生花',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/02%E5%8F%8C%E7%94%9F%E8%8A%B1.flac'
+    },
+    {
+        name: '繁华落尽',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/03%E7%B9%81%E5%8D%8E%E8%90%BD%E5%B0%BD.flac'
+    }, {
+        name: '至此流落天涯',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/04%E8%87%B3%E6%AD%A4%E6%B5%81%E8%90%BD%E5%A4%A9%E6%B6%AF.flac'
+    }, {
+        name: '南山南',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/05%E5%8D%97%E5%B1%B1%E5%8D%97.flac'
+    }, {
+        name: '新航路',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/06%E6%96%B0%E8%88%AA%E8%B7%AF.flac'
+    }, {
+        name: '美しき者',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/07%E7%BE%8E%E3%81%97%E3%81%8D%E8%80%85.flac'
+    }, {
+        name: 'When we are to you',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/08When%20we%20are%20to%20you.flac'
+    }, {
+        name: '南山南',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/09%E5%8D%97%E5%B1%B1%E5%8D%97.flac'
+    },
+    {
+        name: '直到那一天',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/10%E7%9B%B4%E5%88%B0%E9%82%A3%E4%B8%80%E5%A4%A9.flac'
+    },
+    {
+        name: '寒夜',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/11%E5%AF%92%E5%A4%9C.flac'
+    },
+    {
+        name: '蠢蠢',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/12%E8%A0%A2%E8%A0%A2.flac'
+    },
+    {
+        name: '消愁',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/13%E6%B6%88%E6%84%81.flac'
+    },
+    {
+        name: '几分之几',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/14%E5%87%A0%E5%88%86%E4%B9%8B%E5%87%A0.flac'
+    }, {
+        name: '钢铁之翼',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/15%E9%92%A2%E9%93%81%E4%B9%8B%E7%BF%BC.flac'
+    }, {
+        name: '两只老虎',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/16%E4%B8%A4%E5%8F%AA%E8%80%81%E8%99%8E.flac'
+    }, {
+        name: '胡事托',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/17%E8%83%A1%E4%BA%8B%E6%89%98.flac'
+    }, {
+        name: '斑马斑马',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/18%E6%96%91%E9%A9%AC%E6%96%91%E9%A9%AC.flac'
+    }, {
+        name: '降落伞',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/19%E9%99%8D%E8%90%BD%E4%BC%9E.flac'
+    }, {
+        name: '采茶纪',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/20%E9%87%87%E8%8C%B6%E7%BA%AA.flac'
+    },
+    {
+        name: '石中花',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/21%E7%9F%B3%E4%B8%AD%E8%8A%B1.flac'
+    },
+    {
+        name: '等你下课',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/22%E7%AD%89%E4%BD%A0%E4%B8%8B%E8%AF%BE.flac'
+    }, {
+        name: '霜雪千年',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/23%E9%9C%9C%E9%9B%AA%E5%8D%83%E5%B9%B4.flac'
+    },
+    {
+        name: '水星记',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/24%E6%B0%B4%E6%98%9F%E8%AE%B0.flac'
+    },
+    {
+        name: '修炼爱情',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/25%E4%BF%AE%E7%82%BC%E7%88%B1%E6%83%85.flac'
+    }, {
+        name: '美杜莎的温柔',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/26%E7%BE%8E%E6%9D%9C%E8%8E%8E%E7%9A%84%E6%B8%A9%E6%9F%94.flac'
+    }, {
+        name: '我和我的祖国',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/27%E6%88%91%E5%92%8C%E6%88%91%E7%9A%84%E7%A5%96%E5%9B%BD.flac'
+    }, {
+        name: '富士山下',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/28%E5%AF%8C%E5%A3%AB%E5%B1%B1%E4%B8%8B.flac'
+    }, {
+        name: '夜车',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/29%E5%A4%9C%E8%BD%A6.flac'
+    }, {
+        name: '神魂颠倒',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/30%E7%A5%9E%E9%AD%82%E9%A2%A0%E5%80%92.flac'
+    }, {
+        name: '寄明月',
+        url: 'http://7xp06y.com1.z0.glb.clouddn.com/b10/31%E5%AF%84%E6%98%8E%E6%9C%88.flac'
+    }
+];
 
 for (var i = 0; i < playerlist.length; i++) {
     if (playerlist[i].artist === undefined) {
