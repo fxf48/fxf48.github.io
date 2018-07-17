b7fd7e20e51c3f68721af8055dce6758853abd9d
fxf48 <37375451+fxf48@users.noreply.github.com>
2018-07-17 14:23:28 +0800

    fix

--- game/app_v2.js
+++ game/app_v2.js
@@ -2614,5 +2614,5 @@ navigator.userAgent.match(/MSIE\s([\d]+)/) && (isIE10 = !0),
     firstRun = !0,
     hasHorizAction = 1,
     hasVertAction = 1,
-    Howler.mobileAutoEnable = false,
+    Howler.mobileAutoEnable = true,
     loadPreAssets();
